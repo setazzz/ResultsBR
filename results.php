e2e9ec11686873669f2373d0c05cab08e6c7c155
mdominas <mdominas@gmail.com>
2017-08-28 08:57:49 +0300

    v 0.1

--- results.php
+++ results.php
@@ -16,8 +16,8 @@
     <h3 class="date"><?php echo $meta->date;?></h3>
     <a href="index.php">Enter Your result</a>
         <?php include 'resultsTableOutput.php';
-            echo singleTableOutput($numberOfRoutes, $allResults,  $specChal, 'male');
-            echo singleTableOutput($numberOfRoutes, $allResults,  $specChal, 'female');
+            echo singleTableOutput($numberOfRoutes, $allResults, $specChal, 'male');
+            echo singleTableOutput($numberOfRoutes, $allResults, $specChal, 'female');
         ?>
     <a href="results/Results_2017-08-25_BouldeRingas.json" download>Download file</a>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
