a0671d56d460ff0d1359d58b4dcb3ac00e4f9763
mdominas <mdominas@gmail.com>
2017-08-28 09:03:52 +0300

    database connection added

---
+++ inc/connection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matas
+ * Date: 2017.08.18
+ * Time: 12:08
+ */
+
+$user = 'root';
+$pass = '';
+$db = 'resultsbr_database';
+
+$db = new mysqli('localhost', $user, $pass, $db) or die('Unable to connect');
+
